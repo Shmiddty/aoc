33738af745a328e24e316c884a6bd2bd74632c0e
Shmiddty <corey.wade.hart@gmail.com>
2022-03-15 10:14:30 -0600

    2018.22.py now supports printing the cave, which shows which regions the A* algorithm 'explored'

--- python/2018.22.py
+++ python/2018.22.py
@@ -9,7 +9,7 @@ from region import points2d
 def makecave(depth, tx, ty):
     def geoindex(pt):
         x, y = pt
-        if x < 0 or y < 0: return sys.maxint
+        if x < 0 or y < 0: return 0
 
         if pt == (0,0): return 0
         if pt == (tx, ty): return 0
@@ -28,15 +28,32 @@ def makecave(depth, tx, ty):
     merosion = memo(erosion)
     mregiontype = memo(regiontype)
 
-    return mregiontype
+    class Region(int):
+        def __str__(self):
+            return '.=|#'[self]
+
+    class Cave(Grid):
+        def __getitem__(self, key):
+            if key[0] < 0 or key[1] < 0: return 0 # not ideal but does the job.
+
+            if key not in self:
+                self[key] = Region(mregiontype(key))
+
+            return dict.__getitem__(self, key)
+        def get(self, key):
+            return self.__getitem__(key)
+
+    return Cave()
 
 inp = map(int, re.findall(r'\d+', sys.stdin.read()))
 d, tx, ty = inp
-regtype = makecave(d, tx, ty)
+cave = makecave(d, tx, ty)
 
 # part 1
-risk = sum(map(regtype, points2d(((0,0), (tx, ty)))))
+risk = sum(map(cave.get, points2d(((0,0), (tx, ty)))))
 print(risk)
+
+#print(cave)
 
 neither, torch, gear = 0, 1, 2
 rocky, wet, narrow = 0, 1, 2
@@ -53,18 +70,19 @@ path = shortestpath(
     neighbors = lambda (pt, tl): [
         ((x,y), tool)
         for (x,y) in orthogonal(pt)
-        for tool in regtools[regtype((x,y))]
+        for tool in regtools[cave[(x,y)]]
         if x >= 0 and y >= 0 and x <= tx * 5 and
             # x <= tx * 5 is kinda arbitrary and it would be nice
             # to get rid of it in favor of a smartly-expanding area
             # but I haven't been able to figure out how to do that
-            (regtype(pt) == regtype((x,y)) or tl in regtools[regtype((x,y))])
+            tl in regtools[cave[(x,y)]]
     ],
     isblocked = lambda (pt, _): False,
     score = lambda (end, toolEnd): lambda (start, toolStart):(
         manhattan(start, end) + (toolEnd != toolStart) * 7
     )
 )
-
+#print('')
+#print(cave)
 toolswaps = len(partition(map(nth(1), path))) - 1
 print(7 * toolswaps + len(path) - 1)
