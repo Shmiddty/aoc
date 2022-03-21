f1ce8b694847cadb485eba160ca0492de040ad58
Shmiddty <corey.wade.hart@gmail.com>
2022-03-20 23:10:13 -0600

    2020.13.clj - modular inverse and chinese remainder theorem added to utils

--- clojure/2020.13.clj
+++ clojure/2020.13.clj
@@ -1,59 +1,88 @@
 (use '[io :rename {read-all-lines yummy, printI printI}])
 (use '[clojure.string :only (split)])
 (use '[enum :only (enumerate)])
+(use '[util :only (fargo, crt)])
 
 (def inp
-  (->> (yummy)
-       ((fn [[a b]] [
-          (read-string a)
-          (split b #",")
-          ]))
-       ))
+  (apply
+    (fargo read-string #(split % #","))
+    (yummy)
+    ))
 
-; so unpretty
-(->> inp
-     ((fn [[a B]]
-        (->> B
-             (filter #(not= "x" %))
-             (map read-string)
-             (map
-               (fn [b]
-                 (->> (iterate (partial + b) 0)
-                      (drop-while (partial > a))
-                      (first)
-                      (vector b)
-                 )))
-             (apply min-key second)
-             ((fn [[b t]] (* b (- t a))))
-          )
-        ))
-     (println)
-     )
+; so damn unpretty
+;(->> inp
+;     ((fn [[a B]]
+;        (->> B
+;             (filter #(not= "x" %))
+;             (map read-string)
+;             (map
+;               (fn [b]
+;                 (->> (iterate (partial + b) 0)
+;                      (drop-while (partial > a))
+;                      (first)
+;                      (vector b)
+;                 )))
+;             (apply min-key second)
+;             ((fn [[b t]] (* b (- t a))))
+;          )
+;        ))
+;     (println)
+;     )
+
+; slightly less unpretty
+((
+  fn [[a B]] (->> B
+    (filter #(not= "x" %))
+    (map read-string)
+    (map (fn [b] [(mod (- 0 a) b) b]))
+    (apply min-key first)
+    (reduce *)
+    (println)
+  ))
+  inp
+)
+
+;(defn catchup [[one & more]]
+;  (apply conj [(drop-while #(< % (first (last more))) one)] more)
+;  )
+;
+;; still too slow... hmmmmmmm
+;(defn seek [iters]
+;  (if (= 1 (count (set (map first iters))))
+;    (ffirst iters)
+;    (recur (sort-by first (catchup iters)))
+;  ))
 
 ; maybe this is actually a least common multiple problem, kinda.
 ; so is it better to iterate the busses like I was doing originally?
+; it shouldn't be necessary to iterate *all* of them...
+; will the value be less than their cumulative product?
+; so maybe we can work backward from there? not sure if that'd actually be faster
+; it's the chinese remainder theorem. I did this before but forgot all about it.
 (->> inp
      (second)
      (enumerate)
      (filter #(not= "x" (second %)))
-     (map (fn [[i d]] [i (read-string d)])) ; I should maybe make a function for this.
-     ;(map (fn [[i d]] (map #(- % i) (iterate (partial + d) 0))))
-     ((fn [ls]
-        (->> ls
-             (map (fn [[i d]] #(= 0 (mod (+ i %) d))))
-             (apply every-pred)
-             ; let's try to step lively
-             ; looks like this is still too slow.
-             (#(filter
-                 %
-                 (map
-                   (partial + (* -1 (first (apply max-key second ls))))
-                   (iterate (partial + (apply max (map second ls))) 0)
-                   )))
-             ; this is all pretty icky, huh?
-             ; guess it was all a fluke, eh?
-             ; guess my life has no value, right?
-             (first)
-             (printI)
-             )))
+     (map (partial apply (fargo identity read-string)))
+     (map (fn [[i d]] [(mod (- 0 i) d) d]))
+     (crt)
+     (println)
+     ;((fn [ls]
+     ;   (->> ls
+     ;        (map (fn [[i d]] #(= 0 (mod (+ i %) d))))
+     ;        (apply every-pred)
+     ;        ; let's try to step lively
+     ;        ; looks like this is still too slow.
+     ;        (#(filter
+     ;            %
+     ;            (map
+     ;              (partial + (* -1 (first (apply max-key second ls))))
+     ;              (iterate (partial + (apply max (map second ls))) 0)
+     ;              )))
+     ;        ; this is all pretty icky, huh?
+     ;        ; guess it was all a fluke, eh?
+     ;        ; guess my life has no value, right?
+     ;        (first)
+     ;        (printI)
+     ;        )))
      )
--- clojure/lib/util.clj
+++ clojure/lib/util.clj
@@ -1,8 +1,40 @@
 (ns util)
 
+(use '[enum :only (zip)])
+
 (defn untilrepeat [f [state & prev]]
   (if (= state (first prev))
       state
       (untilrepeat f (apply conj [(f state)] state prev))
       ))
+
+; there has to be a better way to write this
+(defn fargo [f0 & fns]
+  (fn [a0 & args]
+    (map
+      #((first %) (second %))
+      (zip (concat [f0] fns) (concat [a0] args))
+      )))
+
+(defn divmod [n d] [(- (/ n d) (/ (mod n d) d)) (mod n d)])
+
+(defn modinv [r m]
+  (loop [[A B t2 t3] [m (mod r m) 0 1]]
+    (if (zero? B)
+      (mod t2 m)
+      (recur [B (mod A B) t3 (- t2 (* t3 (quot A B)))])
+)))
+
+; ls = [[r0 m0] ... [rk mk]]
+; find the value that satisfies all x = rk (mod mk)
+; using the Chinese Remainder Theorem
+(defn crt [ls]
+  ((fn [p]
+    (->> ls
+         (map (fn [[r m]] (* r (/ p m) (modinv (/ p m) m))))
+         (reduce +)
+         (#(mod % p))
+         ))
+   (reduce * (map second ls))
+  ))
 
