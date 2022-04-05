1beef4e3dd09f229fe6f26afa23ca53bc1e288bc
Shmiddty <corey.wade.hart@gmail.com>
2022-04-05 16:42:28 -0600

    2020.23.clj - part 1

---
+++ clojure/2020.23.clj
@@ -0,0 +1,25 @@
+(use '[io :rename {read-all yummy printI dbg}])
+
+(def cups0 (map #(Character/digit % 10) (butlast (yummy))))
+
+(defn move [[A b c d & cups]]
+  (as-> cups C
+    (if (every? (partial < A) C)
+      (split-at (inc (.indexOf C (apply max C))) C)
+      (split-at (inc (.indexOf C (apply max (filter (partial > A) C)))) C)
+      )
+    (apply #(concat %1 [b c d] %2 [A]) C)
+    ))
+
+(->>
+  cups0
+  (iterate move)
+  (take 101)
+  (last)
+  (#(split-at (inc (.indexOf % 1)) %))
+  (reverse)
+  (apply concat)
+  (butlast)
+  (apply str)
+  (println)
+  )
