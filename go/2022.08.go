ea823cca83d689d9bc6c1a6514746189c113a908
Shmiddty <corey.wade.hart@gmail.com>
2022-12-12 21:03:27 -0600

    2022.08.go part 2

--- go/2022.08.go
+++ go/2022.08.go
@@ -47,6 +47,43 @@ func isVisible(row int, col int, theMap [][]int) bool {
   return true
 }
 
+func scenicScore(row int, col int, theMap [][]int) int {
+  wid := len(theMap[0])
+  hei := len(theMap)
+  height := theMap[row][col]
+
+  up := 0
+  for y := row - 1; y >= 0; y-- {
+    up += 1
+    if theMap[y][col] >= height {
+      break
+    }
+  }
+  down := 0
+  for y := row + 1; y < hei; y++ {
+    down += 1
+    if theMap[y][col] >= height {
+      break
+    }
+  }
+  left := 0
+  for x := col - 1; x >= 0; x-- {
+    left += 1
+    if theMap[row][x] >= height {
+      break
+    }
+  }
+  right := 0
+  for x := col + 1; x < wid; x++ {
+    right += 1
+    if theMap[row][x] >= height {
+      break
+    }
+  }
+
+  return up * down * left * right
+}
+
 func main() {
   inp := util.ArgLines()
   theMap := make([][]int, len(inp))
@@ -57,14 +94,20 @@ func main() {
     }
   }
 
-  // part 1
+  // part 1 & 2
   cnt := 0
+  max := 0 // usually you'd do -maxint, but it isn't necessary here
   for x, row := range theMap {
     for y, _ := range row {
       if isVisible(x, y, theMap) {
         cnt += 1
       }
+      score := scenicScore(x, y, theMap)
+      if score > max {
+        max = score
+      }
     }
   }
   println(cnt)
+  println(max)
 }
